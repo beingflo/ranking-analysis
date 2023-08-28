bc09656fee23da3dc3d031244488cc516557ff4e
Florian Marending <florian@marending.dev>
2023-08-28 21:24:02 +0200

    Compute winrate accuracy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use crate::{rankings::win_rate::compute_ranking, util::read_matches};
+use crate::{rankings::win_rate::compute_winrate_accuracy, util::read_matches};
 
 pub mod rankings;
 pub mod util;
 
 fn main() {
     let matches = read_matches();
-    let ranking = compute_ranking(&matches);
+    let winrate_accuracy = compute_winrate_accuracy(&matches);
 
-    for e in ranking.winrate_history.get("251").unwrap() {
-        println!("{{ date: '{}', winrate: {} }},", e.0, e.1);
-    }
+    println!("Winrate accuracy: {}", winrate_accuracy);
 }
--- src/rankings/win_rate.rs
+++ src/rankings/win_rate.rs
@@ -9,13 +9,14 @@ pub struct WinRate {
 
 pub struct WinRateRanking {
     pub winrates: HashMap<String, WinRate>,
-    pub winrate_history: HashMap<String, Vec<(String, f32)>>,
 }
 
-pub fn compute_ranking(matches: &Vec<Match>) -> WinRateRanking {
+pub fn compute_winrate_accuracy(matches: &Vec<Match>) -> f32 {
+    let mut num_matches = 0;
+    let mut correct_predictions = 0;
+
     let mut ranking = WinRateRanking {
         winrates: HashMap::new(),
-        winrate_history: HashMap::new(),
     };
 
     for p in get_players(&matches) {
@@ -26,34 +27,48 @@ pub fn compute_ranking(matches: &Vec<Match>) -> WinRateRanking {
                 num_wins: 0,
             },
         );
-        ranking.winrate_history.insert(p, Vec::new());
     }
 
     for m in matches.iter() {
         let all_players = m.team_a.iter().chain(m.team_b.iter());
-        let winning_team = if m.team_a_score > m.team_b_score {
-            &m.team_a
+        let (winning_team, losing_team) = if m.team_a_score > m.team_b_score {
+            (&m.team_a, &m.team_b)
         } else {
-            &m.team_b
+            (&m.team_b, &m.team_a)
         };
+
+        let winrate_winners: f32 = winning_team
+            .iter()
+            .map(|p| ranking.winrates.get(p).unwrap().get_winrate())
+            .sum();
+        let winrate_losers: f32 = losing_team
+            .iter()
+            .map(|p| ranking.winrates.get(p).unwrap().get_winrate())
+            .sum();
+
+        num_matches += 1;
+
+        if winrate_winners > winrate_losers {
+            correct_predictions += 1;
+        }
 
         for p in winning_team.iter() {
             ranking.winrates.get_mut(p).unwrap().num_wins += 1;
         }
         for p in all_players {
             ranking.winrates.get_mut(p).unwrap().num_matches += 1;
-            ranking.winrate_history.get_mut(p).unwrap().push((
-                m.start_date.clone(),
-                ranking.winrates.get(p).unwrap().get_winrate(),
-            ));
         }
     }
 
-    ranking
+    correct_predictions as f32 / num_matches as f32
 }
 
 impl WinRate {
     pub fn get_winrate(&self) -> f32 {
-        self.num_wins as f32 / self.num_matches as f32
+        if self.num_matches == 0 {
+            0.0
+        } else {
+            self.num_wins as f32 / self.num_matches as f32
+        }
     }
 }
